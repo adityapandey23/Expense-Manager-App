d167fa79668b5b2167b9553df83745065bc68f5a
Aditya Pandey <adityapandey2395@gmail.com>
2024-07-04 11:56:46 +0530

    Added the Homescreen Card and stuff

--- app/src/main/java/com/example/expense_manager_app/HomeScreen.kt
+++ app/src/main/java/com/example/expense_manager_app/HomeScreen.kt
@@ -1,23 +1,30 @@
 package com.example.expense_manager_app
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
@@ -34,7 +41,7 @@ fun HomeScreen(){
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+                    .padding(top = 64.dp, start = 16.dp, end = 16.dp)
                     .constrainAs(nameRow) {
                         top.linkTo(parent.top)
                         start.linkTo(parent.start)
@@ -49,9 +56,71 @@ fun HomeScreen(){
                 Image(
                     painter = painterResource(R.drawable.bell),
                     contentDescription = null,
-                    modifier = Modifier.align(Alignment.CenterEnd))
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                )
             }
+            CardItem(modifier = Modifier.constrainAs(card){
+                top.linkTo(nameRow.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
         }
+    }
+}
+
+@Composable
+fun CardItem(modifier: Modifier){
+    Column(modifier = modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .height(200.dp)
+        .clip(RoundedCornerShape(16.dp))
+        .background(Color.hsl(276F, .30F, .53F))
+        .padding(16.dp)
+    )
+    {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)) {
+            Column(modifier = Modifier.align(Alignment.CenterStart)) {
+                Text(text = "Total Balance", fontSize = 16.sp, color = Color.White)
+                Text(text = "$10,000", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White)
+            }
+            Image(
+                painter = painterResource(R.drawable.threedots),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)) {
+            CardRowItem(
+                modifier = Modifier.align(Alignment.CenterStart),
+                title = "Income",
+                amount = "$3000",
+                image = R.drawable.income
+            )
+
+            CardRowItem(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                title = "Expense",
+                amount = "$2000",
+                image = R.drawable.exp
+            )
+        }
+    }
+}
+
+@Composable
+fun CardRowItem(modifier: Modifier, title: String, amount: String, image:Int){
+    Column(modifier = modifier) {
+        Row {
+            Image(painter = painterResource(id = image), contentDescription = null)
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = title, fontSize = 16.sp, color = Color.White)
+        }
+        Text(text = amount, fontSize = 20.sp, color = Color.White)
     }
 }
 
